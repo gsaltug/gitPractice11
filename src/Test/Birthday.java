99b4dd9a0017aaa01dc0bb05fbca19b1ae1c7730
gsaltug <64171745+gsaltug@users.noreply.github.com>
2020-04-28 16:06:38 -0400

    Update Birthday.java

--- src/Test/Birthday.java
+++ src/Test/Birthday.java
@@ -2,6 +2,6 @@ package Test;
 
 public class Birthday {
     public static void main(String[] args) {
-        System.out.println("Happy Birthday");
+        System.out.println("Happy Birthday Gozde");
     }
 }
